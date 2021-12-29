19429c7ea24dfbe027b730805f9120c92ca0caac
Jackman3016 <63569185+Jackman3016@users.noreply.github.com>
2021-12-30 00:53:34 +0800

    Update VS.hlsl

--- Chili Notes/VS.hlsl
+++ Chili Notes/VS.hlsl
@@ -1,12 +1,18 @@
-struct VSOut {
-	float4 pos: SV_Position;
-	float3 color : COLOR;
+struct VSOut
+{
+    float4 pos : SV_Position;
+    float3 color : COLOR;
+};
+
+cbuffer CBuf
+{
+    row_major matrix transform;//对于二维数组，GPU按列存储，CPU按行存储，除了转置，可以使用关键字告诉GPU按行读取，但GPU按行计算会变慢
 };
 
 VSOut main(float2 pos : POSITION, float3 color : COLOR) : SV_POSITION
 {
-	VSOut vso;
-	vso.pos = float4(posx, pos.y, 0.0f, 1.0f);
-	vso.color = color;
-	return VSOut;
+    VSOut vso;
+    vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);//使用cbuffer不用创建struct
+    vso.color = color;
+    return VSOut;
 }
